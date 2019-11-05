06cd85223f7b59b119ac85c910964da54b8aa1b2
Danuser CI User <danuser_ci@Nucleus007.cm.cluster>
2019-11-05 17:21:20 -0600

    Danuser Lab CI pipeline build # 69686

--- software/@MovieList/buildByRegexp.m
+++ software/@MovieList/buildByRegexp.m
@@ -1,4 +1,4 @@
-function ML = buildByRegexp(filter,outputDirectory,findMatsInDirs)
+function ML = buildByRegexp(filter,outputDirectory)
 % Build a MovieList using a regular expression.
 %
 % INPUT
@@ -35,29 +35,10 @@ function ML = buildByRegexp(filter,outputDirectory,findMatsInDirs)
     if(nargin < 2)
         outputDirectory = pwd;
     end
-    if(nargin < 3)
-        findMatsInDirs = true;
-    end
     D = dir;
-    if(findMatsInDirs)
-        D = D([D.isdir]);
-        D = D(~cellfun(@(x) isempty(regexp(x,filter, 'once')),{D.name}));
-        movieDataFileNames = strcat(pwd,filesep,{D.name},filesep,{D.name},'.mat');
-    else
-        D = D(~cellfun(@(x) isempty(regexp(x,filter, 'once')),{D.name}));
-        movieDataFileNames = {D.name};
-        disp(movieDataFileNames(:));
-        x = input('Create [new] MovieData objects by loading these files Y/N [N]?: ','s');
-        if(isempty(x) || x(1) ~= 'Y')
-            disp('MovieList not created');
-            return;
-        end
-        movieDataFileNames = cellfun(@MovieData.load,{D.name},'Unif',false);
-    end
+    D = D([D.isdir]);
+    D = D(~cellfun(@(x) isempty(regexp(x,filter, 'once')),{D.name}));
+    movieDataFileNames = strcat(pwd,filesep,{D.name},filesep,{D.name},'.mat');
     ML = MovieList(movieDataFileNames,outputDirectory);
-    try
-        ML.sanityCheck;
-    catch err
-        disp(getReport(err));
-    end
+    ML.sanityCheck;
 end
